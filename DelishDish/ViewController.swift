e2b578d5394b003b942aaef6bfcab3ef63f6f0aa
Winnie Wen <wiwen@berkeley.edu>
2016-10-10 19:29:56 -0700

    First commit

---
+++ DelishDish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelishDish
+//
+//  Created by Winnie Wen on 10/10/16.
+//  Copyright © 2016 wiwen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
